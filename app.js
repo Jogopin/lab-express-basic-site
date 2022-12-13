8da58dae33083865b160aed5e76ad3701416a3de
jogopin <jonn.g.pineda@gmail.com>
2022-12-13 17:51:23 +0100

    lab solved, basic structure

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+app.use(express.static(`public`));
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname +"/views/home.html")
+})
+app.get("/about",(req,res)=>{
+    res.sendFile(__dirname +"/views/about.html")
+})
+app.get("/work",(req,res)=>{
+    res.sendFile(__dirname +"/views/work.html")
+})
+
+app.listen(3000)
